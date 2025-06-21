e361f737ed7474c8c5162959dd11e4386a8dd297
LumeCraftors01 <lumecraftors.dev@gmail.com>
2025-06-21 18:02:21 +0800

    Update Loaderv1.lua

--- Loaderv1.lua
+++ Loaderv1.lua
@@ -1,4 +1,4 @@
-local Check = loadstring(game:HttpGet("https://raw.githubusercontent.com/LumeCraftors01/Spacehub/refs/heads/main/Check.lua"))()
+local Check = loadstring(game:HttpGet("https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Check.lua"))()
 
 for PlaceID, Execute in pairs(Check) do
     if PlaceID == game.PlaceId then
