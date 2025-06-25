945675d04bc5b1bf3beaf23e8711d696f6e692f4
LumeCraftors01 <lumecraftors.dev@gmail.com>
2025-06-25 14:15:30 +0800

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -1,5 +1,6 @@
 local Check = {
-      [17574618959] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Just%20a%20baseplate/Baseplate.lua",  -- Name game
+      [17574618959] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Just%20a%20baseplate/Baseplate.lua", 
+      [105141384450654] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Minen/Minen.lua",-- Name game
 }
    
    return Check
