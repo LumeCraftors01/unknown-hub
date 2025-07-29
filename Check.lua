b0d243c154f059c2f902158eca243d8b86e863b9
LumeCraftors01 <lumecraftors.dev@gmail.com>
2025-07-30 03:31:29 +0800

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -2,6 +2,7 @@ local Check = {
       [17574618959] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Just%20a%20baseplate/Baseplate.lua", -- Baseplate 
       [105141384450654] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Minen/Minen.lua", -- Mine
       [104965156633249] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Poop%20%F0%9F%92%A9/poop.lua", -- Poop 💩 
+      [86082995079744] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Slap%20Tower%204%20%F0%9F%91%8B/slaptower4.lua", -- Slap Tower 4 👋 
 }
    
    return Check
