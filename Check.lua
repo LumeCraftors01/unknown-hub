a47b882614fb7f2085c31e6f6c8239cca0bd841b
LumeCraftors01 <lumecraftors.dev@gmail.com>
2025-09-08 10:48:04 +0800

    Update Check.lua

--- Check.lua
+++ Check.lua
@@ -3,7 +3,7 @@ local Check = {
       [105141384450654] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Minen/Minen.lua", -- Mine
       [104965156633249] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Poop%20%F0%9F%92%A9/poop.lua", -- Poop 💩 
       [86082995079744] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Slap%20Tower%204%20%F0%9F%91%8B/slaptower4.lua", -- Slap Tower 4 👋
-      [123537636697650] = "",
+      [123537636697650] = "https://raw.githubusercontent.com/LumeCraftors01/unknown-hub/refs/heads/main/Stud%20Collecting%20Simulator%20%E2%97%BB%EF%B8%8F/source.lua",
 }
    
    return Check
